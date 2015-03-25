1307bfff70b9a15277140377708d0391b4b1fc84
wang1001love <277485574@qq.com>
2015-03-25 18:01:03 +0800

    company

--- test.sql
+++ test.sql
@@ -1,3 +1,4 @@
+//生日问题
 select name ,birthday ,
 	if(cur > today , cur,next) as birth_day
 from (
@@ -25,8 +26,127 @@ CREATE TABLE `sessions` (
   `endtime` time NOT NULL,
   PRIMARY KEY (`id`)
 );
+insert into sessions (app,usr,starttime,endtime)values('app1','user1','08:30','10:30');
+insert into sessions (app,usr,starttime,endtime)values('app1','user2', '08:30','08:45');	
+insert into sessions (app,usr,starttime,endtime)values('app1','user1','09:00','09:30');
+insert into sessions (app,usr,starttime,endtime)values('app1','user2','09:15','10:30');
+insert into sessions (app,usr,starttime,endtime)values('app1','user1','09:15','09:30');
+insert into sessions (app,usr,starttime,endtime)values('app1','user2','10:30','14:30');
+insert into sessions (app,usr,starttime,endtime)values('app1','user1','10:45','11:30');
+insert into sessions (app,usr,starttime,endtime)values('app1','user2','11:00','12:30');
+insert into sessions (app,usr,starttime,endtime)values('app2','user1','08:30','08:45');
+insert into sessions (app,usr,starttime,endtime)values('app2','user2','09:00','09:30');
+insert into sessions (app,usr,starttime,endtime)values('app2','user1','11:45','12:00');
+insert into sessions (app,usr,starttime,endtime)values('app2','user2','12:30','14:30');
+insert into sessions (app,usr,starttime,endtime)values('app2','user1','12:45','13:30');
+insert into sessions (app,usr,starttime,endtime)values('app2','user2','13:00','14:00');
+insert into sessions (app,usr,starttime,endtime)values('app2','user1','14:00','16:30');
+insert into sessions (app,usr,starttime,endtime)values('app2','user2','15:30','17:00');
+//创建索引
+create unique index idx_app_user_s_e_key on sessions(app,usr,starttime,endtime,id);
+create index idx_app_user_s_e on sessions(app,starttime,endtime);
 
-insert into sessions (app,usr,starttime,endtime)values('app1','user1','10:00','10:30');
+(1)标尺重叠
+select a.app , a.usr,a.starttime,a.endtime,b.starttime,b.endtime 
+from sessions as a, sessions as b where a.app=b.app and a.usr=b.usr and (b.starttime <= a.endtime and b.endtime >= a.starttime)
 
+(2)分组重叠
 
+select distinct app,id,usr,starttime as s 
+from sessions as a 
+where not exists (
+	select * from sessions b where 
+		a.app=b.app 
+		and a.usr=b.usr 
+		and a.starttime >b.starttime 
+		and a.starttime <=b.endtime
+)
+select distinct app,id,usr,endtime as e 
+from sessions as a 
+where not exists (
+	select * from sessions b where 
+		a.app=b.app 
+		and a.usr=b.usr 
+		and a.endtime >=b.starttime 
+		and a.endtime <b.endtime
+)
 
+select DISTINCT s.app ,s.usr,s.s,
+(
+	select MIN(e)
+	FROM( 
+		select distinct app,id,usr,endtime as e 
+		from sessions as a 
+		where not exists (
+			select * 
+			from 
+			sessions b 
+			where 
+			a.app=b.app 
+			and a.usr=b.usr 
+			and a.endtime >=b.starttime 
+			and a.endtime <b.endtime
+		)
+	) AS s2
+	where s2.e > s.s and s.app = s2.app and s.usr=s2.usr
+) as e
+FROM
+(
+	select distinct app,id,usr,starttime as s 
+	from sessions as a 
+		where not exists (
+			select * 
+			from 
+			sessions b where 
+			a.app=b.app 
+			and a.usr=b.usr 
+			and a.starttime >b.starttime 
+			and a.starttime <=b.endtime
+		)
+
+) AS s,
+(
+	select distinct app,id,usr,endtime as e 
+	from sessions as a 
+	where not exists (
+		select * 
+		from sessions b 
+		where 
+		a.app=b.app 
+		and a.usr=b.usr 
+		and a.endtime >=b.starttime 
+		and a.endtime <b.endtime
+	)
+
+)
+as e
+where s.app =e.app and s.usr =e.usr;	
+
+(3)最大重叠会话数
+
+select app,MAX(count) as max 
+FROM(
+	select app, s,
+	(
+		select count(1) 
+		from 
+		sessions as b 
+		where 
+		s>=starttime 
+		and s<endtime
+	)as count
+	FROM (
+			select 
+			DISTINCT app,starttime as s 
+			FROM 
+			sessions 
+		) as a
+
+)AS bc
+group by app;
+
+//星期几问题
+1）计算日期是星期几
+set @a='2011-01-01';
+select WEEKDAY(@a),DAYOFWEEK(@a),DAYNAME(@a);
+set lc_time_names='zh_CN';//控制 DAYNAME显示中文星期
